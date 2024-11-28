1471f255f63cdedaf287ec9de208c7cd1eecc60f
──「 ʟᴜᴄɪғᴇʀ 」── <nimovie08@gmail.com>
2024-11-28 06:40:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ session_name_bot = "sessions/bot"
 
 # Follow this guide to obtain your API credentials: 
 # https://core.telegram.org/api/obtaining_api_id
-API_ID = ""
-API_HASH = ""
+API_ID = "25061703"
+API_HASH = "744a017a9c53f3ab489ea0bfa0ffce3f"
 
 # Write to @BotFather in Telegram to create a bot token
-BOT_TOKEN = ""
+BOT_TOKEN = "7711608312:AAGnkhxkCOe96KKvzJZJBzUqBDPKyJkxv2E"
 
 # The ID of the OpenAI model to use for generating responses.
 model_engine = "gpt-3.5-turbo"
